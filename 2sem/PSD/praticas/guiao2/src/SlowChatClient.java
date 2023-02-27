c6c152759c82473ea200383a87e582841101b1f0
Mariii-23 <marianadinis8@hotmail.com>
2023-02-27 10:48:43 +0000

    Add ABD teoricas

---
+++ 2sem/PSD/praticas/guiao2/src/SlowChatClient.java
@@ -0,0 +1,30 @@
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SocketChannel;
+import java.nio.charset.StandardCharsets;
+
+public class SlowChatClient {
+    public static void main(String[] args) throws IOException {
+        SocketChannel s = SocketChannel.open(new InetSocketAddress("localhost", 12345));
+
+        s.write(ByteBuffer.wrap("hello\n".getBytes()));
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            System.out.println(e.getMessage());
+        }
+        s.write(ByteBuffer.wrap("Wait\n".getBytes()));
+        ByteBuffer bb = ByteBuffer.allocate(1);
+        while (s.read(bb) > 0) {
+            System.out.println(new String(bb.array()));
+            bb.clear();
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+}
+
