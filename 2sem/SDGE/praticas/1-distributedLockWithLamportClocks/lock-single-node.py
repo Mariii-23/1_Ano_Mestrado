f3cca2de2b04a9ab19330903dbb048a00250194f
Mariii-23 <marianadinis8@hotmail.com>
2023-03-23 15:20:16 +0000

    Fix lock-single-node -> SDGE

--- 2sem/SDGE/praticas/1-distributedLockWithLamportClocks/lock-single-node.py
+++ 2sem/SDGE/praticas/1-distributedLockWithLamportClocks/lock-single-node.py
@@ -1,24 +1,27 @@
 #!/usr/bin/env python
-
-"""Single node (centralized) lock for Maelstrom
-
-For 'lock' workload.  Will pass the test using
-"--node-count 1", but will fail if using, .e.g.,
-"--node-count 3".
-"""
-
 import logging
-from ms import receiveAll, reply
+from ms import receiveAll, reply, send
 
 acquired = None
 requests = []
-
 timestamp = 0
+number_clients = 0
 
+# pedir a todas as réplicas o lock
 def notify_clients(client_id,timestamp):
+    global number_clients
     for client in node_ids:
         if client != client_id:
-            send(client_id, client, type="lock", timestamp=timestamp)
+            send(client_id, client, type="request_lock", timestamp=timestamp)
+            number_clients += 1
+
+# pedir a todas as réplicas o unlock
+def notify_clients_unlock(client_id,timestamp):
+    global number_clients
+    for client in node_ids:
+        if client != client_id:
+            send(client_id, client, type="request_unlock", timestamp=timestamp)
+            number_clients += 1
 
 for msg in receiveAll():
     match msg.body.type:
@@ -27,22 +30,62 @@ for msg in receiveAll():
             node_ids = msg.body.node_ids
             logging.info('node %s initialized', node_id)
             reply(msg, type='init_ok')
+
         case 'lock':
-            if not acquired:
+            if acquired == None:
+                timestamp += 1
                 notify_clients(node_id, timestamp)
-                reply(msg, type='lock_ok')
+
                 acquired = msg
+                if number_clients == 0:
+                    reply(msg, type='lock_ok')
             else:
                 requests.append(msg)
+
         case 'unlock':
-            reply(msg, type='unlock_ok')
+            timestamp += 1
             if acquired and acquired.src == msg.src:
+                notify_clients_unlock(node_id,timestamp)
+                reply(msg, type='unlock_ok')
+
                 if requests:
                     acquired = requests.pop(0)
                     notify_clients(node_id,timestamp)
-                    reply(acquired, type='lock_ok')
+
+                    if number_clients == 0:
+                        reply(acquired, type='lock_ok')
                 else:
                     acquired = None
+                acquired = None
+
+        case 'request_lock':
+            #TODO: fix me no caso de receber um request lock
+            #e eu próprio ter um lock
+            if msg.body.timestamp > timestamp:
+                timestamp = msg.body.timestamp
+
+            acquired = msg
+            timestamp += 1
+            reply(msg, type='request_lock_ok', timestamp=timestamp)
+
+        case 'request_lock_ok':
+            number_clients -= 1
+
+            timestamp += 1
+            if number_clients == 0:
+                reply(acquired, type='lock_ok')
 
 
+        case 'request_unlock':
+            if msg.body.timestamp > timestamp:
+                timestamp = msg.body.timestamp
 
+            acquired = None
+            if requests:
+                acquired = requests.pop(0)
+                notify_clients(node_id,timestamp)
+            timestamp += 1
+            reply(msg, type='request_unlock_ok', timestamp=timestamp)
+
+        case 'request_unlock_ok':
+            timestamp += 1
